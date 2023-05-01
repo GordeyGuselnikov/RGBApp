7c0bfcfb5f2b59ffcd7c444a390140ee6eef5267
Gordey Guselnikov <g.guselnikov@bi.zone>
2023-05-02 02:29:57 +0700

    first commit

--- RGBApp/ViewController.swift
+++ RGBApp/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var viewRGB: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateBackgroundColor()
     }
 
-
+    override func viewWillLayoutSubviews() {
+        viewRGB.layer.cornerRadius = 10
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    @IBAction func slidersAction(_ sender: UISlider) {
+        switch sender {
+        case redSlider: setValue(for: redLabel, from: redSlider)
+        case greenSlider: setValue(for: greenLabel, from: greenSlider)
+        default: setValue(for: blueLabel, from: blueSlider)
+        }
+        updateBackgroundColor()
+    }
+    
+    private func updateBackgroundColor() {
+        viewRGB.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func setValue(for label: UILabel, from slider: UISlider) {
+            label.text = string(from: slider)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+            String(format: "%.2f", slider.value)
+    }
 }
-
